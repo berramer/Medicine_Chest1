41b5cbc25f31feefd31ba10dc879455a310e8851
Cebrail Bozdağ <cebrail.bozdag@agarthateknoloji.com.tr>
2022-05-21 22:11:18 +0300

    comti2

---
+++ ENTITIES/BUCKET.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITIES
+{
+   public class BUCKET:EntityBase
+    {
+        public string MedicineId;
+        public string UserId;
+        public decimal Price;
+        public int Item;
+
+    }
+}
