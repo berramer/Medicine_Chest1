65030ec71fdde7cb3b9ae691e955ef8a92b68ee7
Cebrail Bozdağ <cebrail.bozdag@agarthateknoloji.com.tr>
2022-05-31 17:23:02 +0300

    commit 33

---
+++ Medicine_Chest/Models/ResetPasswordModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Medicine_Chest.Models
+{
+    public class ResetPasswordModel
+    {
+        public string userId { get; set; }
+        public string token { get; set; }
+        public string password { get; set; }
+    }
+}
